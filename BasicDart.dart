90a30d9c47a2ac6815335637ff29d0763345e98a
IHAMZAAHMADKHAN <ihamzahmadkhan@gmail.com>
2025-02-01 21:27:47 +0500

    Initial commit

---
+++ BasicDart.dart
@@ -0,0 +1,28 @@
+//Dart Program To Join One Or More Variables
+
+// void main() {
+//   var name = "Hamza";
+//   var middle = "Ahmad";
+
+//   print("Full Name: $name $middle");
+// }
+
+//Dart Program For Basic Calculation
+
+void main() {
+  int num1 = 10; //declaring number1
+  int num2 = 3; //declaring number2
+
+// Calculation
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div =
+      num1 / num2; // It is double because it outputs number with decimal.
+
+// displaying the output
+  print("The sum is $sum");
+  print("The diff is $diff");
+  print("The mul is $mul");
+  print("The div is $div");
+}
